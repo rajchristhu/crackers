7774bf996418b2c5fce0cc1dc6c551c574c856db
christhu <rajchristhu39@gmail.com>
2020-10-18 18:29:10 +0530

    Done

--- app/src/main/java/com/ceseagod/rajarani/fragment/MainFragment.kt
+++ app/src/main/java/com/ceseagod/rajarani/fragment/MainFragment.kt
@@ -26,11 +26,12 @@ import org.jetbrains.anko.startActivity
 
 class MainFragment(val mainActivity: MainActivity) : Fragment() {
     var cateModel: CateModel? = null
-    var cateModels= mutableListOf<CateModel>()
+    var cateModels = mutableListOf<CateModel>()
 
     companion object {
 //        fun newInstance() = MainFragment(this)
     }
+
     var firestoreDB: FirebaseFirestore? = null
     var cartList: MutableList<Cart> = mutableListOf<Cart>()
 
@@ -47,11 +48,10 @@ class MainFragment(val mainActivity: MainActivity) : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        firestoreDB=FirebaseFirestore.getInstance()
+        firestoreDB = FirebaseFirestore.getInstance()
 
         imageView7.setOnClickListener {
-            if (prices.text.toString()!="0 ₹")
-            {
+            if (prices.text.toString() != "0 ₹") {
                 mainActivity.startActivity<CartItemActivity>()
                 mainActivity.finish()
             }
@@ -59,8 +59,7 @@ class MainFragment(val mainActivity: MainActivity) : Fragment() {
         }
         try {
             imaged.visibility = View.VISIBLE
-            imaged.setAnimation("pre.json")
-
+            imaged.setAnimation("oks.json")
             imaged.playAnimation()
             imaged.loop(true)
         } catch (e: Exception) {
@@ -74,41 +73,42 @@ class MainFragment(val mainActivity: MainActivity) : Fragment() {
 //            activity!!.head.text="Support"
         } catch (e: Exception) {
         }
-        imaged.visibility=View.VISIBLE
-        firestoreDB!!.collection("categories").orderBy("created_time", Query.Direction.DESCENDING).get()
+        imaged.visibility = View.VISIBLE
+        firestoreDB!!.collection("categories").orderBy("created_time", Query.Direction.DESCENDING)
+            .get()
             .addOnSuccessListener { document ->
                 cateModels.clear()
-                for (i in document)
-                {
+                for (i in document) {
                     cateModel = i.toObject(CateModel::class.java)
                     cateModels!!.add(cateModel!!)
-                    val filter=cateModels!!.filter { it.type=="main" }
-                    val filters=cateModels!!.filter { it.type=="sub" }
+                    val filter = cateModels!!.filter { it.type == "main" }
+                    val filters = cateModels!!.filter { it.type == "sub" }
                     val acceptHorizontalLayoutsss =
                         LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
                     try {
                         recyclerViewone!!.layoutManager = acceptHorizontalLayoutsss
-                        recyclerViewone!!.adapter = MainAdapter( activity!!,filter)
-                        recyclerViewtwo!!.layoutManager =   LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
-                        recyclerViewtwo!!.adapter = SecondAdapter( activity!!,filters)
+                        recyclerViewone!!.adapter = MainAdapter(activity!!, filter)
+                        recyclerViewtwo!!.layoutManager =
+                            LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+                        recyclerViewtwo!!.adapter = SecondAdapter(activity!!, filters)
                     } catch (e: Exception) {
                     }
 
                 }
                 view()
-                imaged.visibility=View.GONE
+                imaged.visibility = View.GONE
 
             }
             .addOnFailureListener {
-                imaged.visibility=View.GONE
+                imaged.visibility = View.GONE
 
 //                    toppy.visibility=View.GONE
             }
 
 
     }
-    private fun view()
-    {
+
+    private fun view() {
         viewModel?.allWords?.observe(this,
             Observer<List<Cart>> { t ->
                 cartList.clear()
@@ -123,9 +123,7 @@ class MainFragment(val mainActivity: MainActivity) : Fragment() {
                         price += i.price.toInt()
                     }
                     prices.text = "$price ₹"
-                }
-                else
-                {
+                } else {
                     prices.text = "0 ₹"
 
                 }
